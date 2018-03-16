59c32d8b5c5c6c5d72d9d6f9c6391faa253547f6
Kelley Robinson <krobinson@twilio.com>
2018-03-16 11:19:14 -0700

    remove encoding

--- app.py
+++ app.py
@@ -8,12 +8,16 @@ app.config.from_object('app_config')
 @app.route("/sms", methods=["GET", "POST"])
 def sms():
     from_number = request.values.get("From")
-    incoming_message = request.values.get("Body", "").encode("utf-8")
+    incoming_message = request.values.get("Body", "")
 
     twiml = """
     <Response>
         <Message>
-            Hello! I see you're texting me from: {}\n\nHere's the message I received:\n\n{}
+Hello! I see you're texting me from: {}
+
+Here's the message I received:
+
+{}
         </Message>
     </Response>
     """.format(from_number, incoming_message)
